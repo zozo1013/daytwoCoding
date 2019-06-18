30a4e0e7b327761cb40a1ea37adbfc5822184848
Zoe Punjwalni <zoepunjwani@gmail.com>
2019-06-18 16:45:48 -0400

    Day Two: KWK

---
+++ Contents.swift
@@ -0,0 +1,111 @@
+//class FacebookProfile {
+//    //we are going to design this class first before we even touch the computer!
+//
+//    //Attributes section: what a FB profile is
+//
+//    var bio: String
+//    var friendCount: Int
+//    var userName: String
+//    var birthday: String
+//    var relationStatus: String
+//    var friendList: [String]
+//
+//    //preselected options for relationship status
+//    let optionOne = "Single"
+//    let optionTwo = "It's Complicated"
+//    let optionThree = "Married"
+//    let optionFour = "Divorced"
+//
+//    //functions that lets user update their bio
+//    func setBio(userInput: String){
+//        bio = userInput
+//    }
+////function that lets user update their FB name
+//    func setUsername(userInput: String) {
+//        userName = userInput
+//    }
+//
+////function that lets user update friend count
+//    func setFriend() {
+//        var friendsNum = friendList.count
+//        friendCount = friendsNum
+//    }
+//
+////function that updates the profiles Bday
+//    func setBirthday (userInput: String){
+//        birthday = userInput
+//    }
+//
+////function that updates the profiles relationship status
+//    // what goes into this function: what data type should user input be
+//    func setrelationStatus(userInput: Int){
+//        if userInput == 1 {
+//            relationStatus == optionOne
+//        }
+//        else if userInput == 2 {
+//            relationStatus = optionTwo
+//        }
+//        else if userInput == 3 {
+//            relationStatus = optionThree
+//        }
+//        else if userInput == 4 {
+//            relationStatus = optionFour
+//        }
+//        else  {
+//            print("Invalid")
+//        }
+//    }
+//
+//
+//// wrapper functions: lets user check their profile info
+//func checkBio() -> String {
+//    return bio
+//    }
+////
+//////function that lets user check friend count
+//func checkFriendCount() -> Int {
+//    return friendCount
+//    }
+////
+//////function that lets user check username
+//   func checkUserName() -> String {
+//        return userName
+//    }
+//
+//// function that lets user check birthday
+//    func checkBirthday() -> String {
+//        return birthday
+//    }
+//
+//// function that lets user check relation status
+//    func checkRelationStatus() -> String {
+//        return relationStatus
+//    }
+//
+//
+////init section init -> initializer
+//// tells computer how to build an object of the class
+//init() {
+//    bio = " "
+//    friendCount = 0
+//    userName = " "
+//    birthday = " "
+//    relationStatus = optionTwo
+//    friendList = [String]()
+//}
+//}
+//
+////testing FB profiles
+//var zoeProfile = FacebookProfile()
+//zoeProfile.setUsername(userInput: "Zoe")
+//zoeProfile.setrelationStatus(userInput: 2)
+//zoeProfile.setBirthday(userInput: "1013")
+//
+//
+//zoeProfile.checkUserName()
+//zoeProfile.checkRelationStatus()
+//
+
+
+
+
